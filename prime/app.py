31e1bea643102158b518cee180e3e07c689f6bf4
Ali <mojtabavi99@yahoo.com>
2020-04-13 21:48:06 +0430

    Algorithm Changed XD

--- prime/app.py
+++ prime/app.py
@@ -2,27 +2,16 @@
 from math import sqrt,floor
 import sys
 
-file_name = sys.argv[1]
+#file_name = sys.argv[1]
 
 def is_prime2(num: int) -> bool:
-    if num <= 1:
-        return False
-
-    if num == 2:
+    if ((num +1 ) % 6 == 0 or num == 2 or num == 3 or (num - 1) % 6 == 0):
         return True
-
-    if num > 2 and num % 2 == 0:
-        return False
-
-    max_div = floor(sqrt(num))
-    for i in range(3, 1 + max_div, 2):
-        if num % i == 0:
-            return False
-
-    return True
+    else:
+        return False 
 
 
-with open(file_name) as input_numbers:
+with open('number.txt') as input_numbers:
     for line in input_numbers:
         number = int(line)
         print(1 if is_prime2(number) else 0)
