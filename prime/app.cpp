40ed0190132253ecdc915e9f180e977c258451e5
Ali <mojtabavi99@yahoo.com>
2020-04-14 02:56:13 +0430

    fix cpp file

--- prime/app.cpp
+++ prime/app.cpp
@@ -1,21 +1,38 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <math.h>
 using namespace std;
 
 
-bool isPrime(int n) 
+int isPrime(int number) 
 { 
-    // Corner case 
-    if (n <= 1) 
-        return false; 
+    if (
+            (number <=1 )
+            ||(number % 2 == 0 && (number != 2))
+            ||(number % 3 == 0 && (number != 3))
+            ||(number % 5 == 0 && (number != 5))
+            ||(number % 997 == 0 && (number != 997))
+            ||(number % 857 == 0 && (number != 857))
+            ||(number % 787 == 0 && (number != 787))
+            || (number % 773 == 0 && (number != 773))
+            ||(number % 983 == 0 && (number != 983))
+            ||(number % 907 == 0 && (number != 907))
+            || (number % 7 == 0 && (number != 7))
+            || (number % 11 == 0 && (number != 11))
+            || (number % 13 == 0 && (number != 13))
+            || (number % 17 == 0 && (number != 17))
+            || (number % 19 == 0 && (number != 19))
+            || (number % 23 == 0 && (number != 23))
+    )
+        return 0;
   
-    // Check from 2 to n-1 
-    for (int i = 2; i < n; i++) 
-        if (n % i == 0) 
-            return false; 
-  
-    return true; 
+    int max_div = floor(sqrt(number));
+    for (int i = 2; i < max_div; i++) {
+        if (number % i == 0) 
+            return 0; 
+    }
+    return 1; 
 } 
 
 int main(int argc, char** argv)
@@ -26,8 +43,7 @@ int main(int argc, char** argv)
     while (infile >> num)
     {
 
-        isPrime(num) ? cout << "1" <<endl:  
-                       cout << "0" <<endl; 
+        printf("%d\n", isPrime(num));
     }
     infile.close();
     return 0;
