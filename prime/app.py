6795b6129ceefef537d0bd6b598c519610c2aac2
Ali <mojtabavi99@yahoo.com>
2020-04-14 01:22:44 +0430

    change to default code

--- prime/app.py
+++ prime/app.py
@@ -5,22 +5,21 @@ import sys
 file_name = sys.argv[1]
 
 def is_prime2(num: int) -> bool:
-    return False
-#    if num <= 1:
- #       return False
-#
- #   if num == 2:
-  #      return True
-#
- #   if num > 2 and num % 2 == 0:
-    #      return False
-#
- #   max_div = floor(sqrt(num))
-  #  for i in range(3, 1 + max_div, 2):
-   #     if num % i == 0:
-    #        return False
+    if num <= 1:
+        return False
 
-    #return True
+    if num == 2:
+        return True
+
+    if num > 2 and num % 2 == 0:
+        return False
+
+    max_div = floor(sqrt(num))
+    for i in range(3, 1 + max_div, 2):
+        if num % i == 0:
+            return False
+
+    return True
 
 with open(file_name) as input_numbers:
     for line in input_numbers:
