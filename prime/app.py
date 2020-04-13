035f2bd324d416f418bc43ad206667cefbbb9a3a
Ali <mojtabavi99@yahoo.com>
2020-04-13 23:07:11 +0430

    back to old algo

--- prime/app.py
+++ prime/app.py
@@ -5,11 +5,21 @@ import sys
 file_name = sys.argv[1]
 
 def is_prime2(num: int) -> bool:
-    if ((num +1 ) % 6 == 0 or num == 2 or num == 3 or (num - 1) % 6 == 0):
-        return True
-    else:
-        return False 
+    if num <= 1:
+        return False
 
+    if num == 2:
+        return True
+
+    if num > 2 and num % 2 == 0:
+        return False
+
+    max_div = floor(sqrt(num))
+    for i in range(3, 1 + max_div, 2):
+        if num % i == 0:
+            return False
+
+    return True
 
 with open(file_name) as input_numbers:
     for line in input_numbers:
