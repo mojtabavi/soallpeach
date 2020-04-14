c8b2b7f59da02f9ff3c0356ace37ea0106bb2d55
Ali <mojtabavi99@yahoo.com>
2020-04-14 17:28:42 +0430

    improve performance

---
+++ prime/main/app.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <math.h>
+using namespace std;
+
+int m=0;
+int num=0;
+int isPrime(int n)
+{
+    if (n <= 3)
+        return 1;
+    if (n % 2 == 0 || n % 3 == 0)
+        return 0;
+
+    for (m = 5; m < (int)sqrt(n) + 1; m = m + 6)
+    {
+        if (n % m == 0 || n % (m + 2) == 0)
+            return 0;
+    }
+    return 1;
+    
+}
+
+
+
+
+int main(int argc, char** argv)
+{
+    ifstream infile(argv[1]);
+    while (infile >> num)
+    {
+
+        printf("%d\n", isPrime(num));
+    }
+
+    
+    infile.close();
+    return 0;
+}
