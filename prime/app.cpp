528e7221eff52b5e6b021e55cb4d293cf7a85cd7
= <mojtabavi99@yahoo.com>
2020-04-24 00:36:40 +0430

    Algorithm Changed

--- prime/app.cpp
+++ prime/app.cpp
@@ -14,6 +14,10 @@ using namespace std;
 
 int m=0;
 int num=0;
+#define limit 100000
+#define wlimit 317 // sqrt(limit)
+
+unsigned char numbers[limit];
 
 
 static int isPrime(int n)
@@ -38,6 +42,45 @@ static int isPrime(int n)
 
 int main(int argc, char** argv)
 {
+
+    int x, y, z, h, g, j;
+    for (x=1; x<wlimit; x++)
+        for (y=1; y<wlimit; y++) {
+            h = x*x;
+            g = y*y;
+            z = (h << 2) + g;
+            if (z < limit) {
+                j = z % 60;
+                if (j == 1 || j == 13 || j == 17 || j == 29 ||
+                    j == 37 || j == 41 || j == 49 || j == 53)
+                    numbers[z] = !numbers[z];
+            }
+            z = z - h;
+            if (z < limit) {
+                j = z % 60;
+                if (j == 7 || j == 19 || j == 31 || j == 43)
+                    numbers[z] = !numbers[z];
+            }
+            z = z - (g << 1);
+            if (x > y && z < limit) {
+                j = z % 60;
+                if (j == 11 || j == 23 || j == 47 || j == 59)
+                    numbers[z] = !numbers[z];
+            }
+        }
+
+    for (x=7; x<wlimit; x++)
+        if (numbers[x] == 1) {
+            z = x*x;
+            for (y=1; y*z<limit; y++)
+                numbers[y*z] = 0;
+        }
+
+    numbers[1] = numbers[4] = 0;
+    numbers[2] = numbers[3] = numbers[5] = 1;
+
+
+
     int n = 0;
     struct stat fs;
     int fid = open(argv[1], O_RDONLY | O_DIRECT);
@@ -57,7 +100,13 @@ int main(int argc, char** argv)
             break;
 
         // check primality of n
-        printf("%d\n" , isPrime(n));
+        if (n<limit)
+            x = numbers[n];
+        else {
+            x = isPrime(n);
+        }
+        
+        printf("%d\n" , x);
 
         n = 0;
     } while (1);
